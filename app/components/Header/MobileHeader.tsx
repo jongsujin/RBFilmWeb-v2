8c83296c0d50a0d70d244671ed396513ca3e0123
unknown <jjs9836@naver.com>
2024-06-16 01:34:04 +0900

    feat: first commit

---
+++ app/components/Header/MobileHeader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import DeskTopHeader from "./DeskTopHeader";
+
+export default function MobileHeader() {
+  return (
+    <header>
+      <DeskTopHeader />
+      <h1>모바일 헤더</h1>
+    </header>
+  );
+}
