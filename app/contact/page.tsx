70b680e6c01c261d02d5a4920e0a988f45af3123
unknown <jjs9836@naver.com>
2024-08-03 17:35:50 +0900

    Feat: 데스크탑 견적 문의 퍼블리싱

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,16 @@
 import Footer from "../../components/Footer/Footer";
+import DeskTopContact from "./_component/DeskTopContact";
 import MobileContact from "./_component/MobileContact";
 
 export default function Contact() {
   return (
     <>
-      <MobileContact />;
+      <div className="block lg:hidden">
+        <MobileContact />;
+      </div>
+      <div className="hidden lg:block">
+        <DeskTopContact />
+      </div>
       <Footer />;
     </>
   );
