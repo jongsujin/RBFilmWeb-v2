bc2f99d289b33c3ef8227340f1897ddeb65c272b
unknown <jjs9836@naver.com>
2024-08-09 00:32:13 +0900

    feat: 데스크탑 포토 상세 페이지 퍼블리싱

--- app/photo/item/[id[]/page.tsx
+++ app/photo/item/[id[]/page.tsx
@@ -1,8 +1,8 @@
 import MobileHeader from "@/components/Header/MobileHeader";
 import DeskTopHeader from "@/components/Header/DeskTopHeader";
+import Footer from "@/components/Footer/Footer";
 import DeskTopPhotoItem from "./_component/DeskTopPhotoItem";
 import MobilePhotoItem from "./_component/MobilePhotoItem";
-import Footer from "@/components/Footer/Footer";
 
 export default function PhotoItem() {
   return (
