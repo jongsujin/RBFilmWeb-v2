e6c330bde291cc90fb8c27061e412e552d19a486
unknown <jjs9836@naver.com>
2024-09-01 23:48:39 +0900

    Feat: 포토 페이지 푸터 추가

--- app/photo/page.tsx
+++ app/photo/page.tsx
@@ -1,9 +1,19 @@
+import DeskTopFooter from "@/components/Footer/DeskTopFooter";
+import Footer from "@/components/Footer/Footer";
 import MobilePhoto from "./_component/MobilePhoto";
 
 export default function Photo() {
   return (
-    <div>
-      <MobilePhoto />
-    </div>
+    <>
+      <div>
+        <MobilePhoto />
+      </div>
+      <div className="block lg:hidden">
+        <Footer />
+      </div>
+      <div className="hidden lg:block lg:mt-72">
+        <DeskTopFooter />
+      </div>
+    </>
   );
 }
